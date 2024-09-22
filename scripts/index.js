a68e18714484a0d7b35074582c99dc115e873c80
RuslanWeber <ruslan26812@gmail.com>
2024-09-22 17:42:34 +0300

    Update index.js

--- scripts/index.js
+++ scripts/index.js
@@ -2,17 +2,19 @@ const cardList = document.querySelector('.places__list');
 function createCard(cardData, deleteCard) {
     const cardTemplate = document.querySelector('#card-template').content;
     const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
-    cardElement.querySelector ('.card__image').src = cardData.link;
+    const cardImage = cardElement.querySelector ('.card__image');
+    cardImage.src = cardData.link;
+    cardImage.alt = cardData.name;
     cardElement.querySelector('.card__title').textContent = cardData.name;
     const deleteButton = cardElement.querySelector('.card__delete-button');
     if (deleteButton) {
-        deleteButton.addEventListener('click', deleteCard);
+        deleteButton.addEventListener('click', () => deleteCard(cardElement));
     }
     return cardElement;
 };
 
-function deleteCard(evt){
-    evt.target.closest('.card').remove();
+function deleteCard(card){
+    card.remove();
 };
 
 function addCards() {
