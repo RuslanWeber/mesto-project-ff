8c9d6e4296c519b0febdd5d24092216f29f0f0d0
RuslanWeber <ruslan26812@gmail.com>
2024-09-22 16:42:46 +0300

    Update index.js

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,25 @@
-// @todo: Темплейт карточки
+const cardList = document.querySelector('.places__list');
+function createCard(cardData, deleteCard) {
+    const cardTemplate = document.querySelector('#card-template').content;
+    const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
+    cardElement.querySelector ('.card__image').src = cardData.link;
+    cardElement.querySelector('.card__title').textContent = cardData.name;
+    const deleteButton = cardElement.querySelector('.card__delete-button');
+    if (deleteButton) {
+        deleteButton.addEventListener('click', deleteCard);
+    }
+    return cardElement;
+};
 
-// @todo: DOM узлы
+function deleteCard(evt){
+    evt.target.closest('.card').remove();
+};
 
-// @todo: Функция создания карточки
+function addCards() {
+    for (let i = 0; i < initialCards.length; i++) {
+        cardList.append(createCard(initialCards[i], deleteCard));
+    }
+};
 
-// @todo: Функция удаления карточки
 
-// @todo: Вывести карточки на страницу
+addCards();
